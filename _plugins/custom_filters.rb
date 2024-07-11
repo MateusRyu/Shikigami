4010277442a3ec12eb68ccebb7f4544d00d7ba3f
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-07-11 00:59:19 -0300

    vault backup: 2024-07-11 00:59:19
    Affected files:
    _plugins/custom_filters.rb

--- _plugins/custom_filters.rb
+++ _plugins/custom_filters.rb
@@ -1,7 +1,7 @@
 module CustomFilters
-  def erase_md_extension(input)
+  def remove_md_extension(input)
     input.gsub(/\.md\)/, ')')
   end
 end
 
-Liquid::Template.register_filter(CustomFilters)
+Liquid::Template.register_filter(Jekyll::LinkFilter)
