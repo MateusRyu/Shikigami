c318685b79c83929c965afe2f0a16ecada689707
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-08-02 14:57:33 -0300

    feat: Improve acessibility to navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,16 @@
-function toggleElement(id) {
-  document.getElementById(id).classList.toggle("show");
+function toggleElement(id, className) {
+  document.getElementById(id).classList.toggle(className);
 }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropButton')) {
+window.onclick = function (event) {
+  if (!event.target.matches(".dropButton")) {
     const dropdowns = document.getElementsByClassName("dropdown-content");
     for (let i = 0; i < dropdowns.length; i++) {
       const openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
-}
+};
+
